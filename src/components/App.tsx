61cd98b659eb7bb7ce2e0a66afa39fcb33c8baab
樊沛 <fanpei920@gmail.com>
2019-01-08 00:33:13 +0900

    fix the dev config

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { SelectModal } from "./SelectModal/index";
+
+export const App = () => 
+  <div>
+    <SelectModal />
+  </div>
---
+++ src/components/SelectModal/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import "./index.scss"
+
+export class SelectModal extends React.Component<any> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      modalActivated: true,
+    };
+  }
+
+  render () {
+    return (
+      <div className="modal">
+        ojbk!!!!222112111122
+      </div>
+    )
+  }
+}
