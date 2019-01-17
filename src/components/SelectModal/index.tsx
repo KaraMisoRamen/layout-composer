bb1535cf239d29cbb464f15070eae350b573cb5c
樊沛 <fanpei920@gmail.com>
2019-01-18 00:07:47 +0900

    added modal event

--- src/components/SelectModal/index.tsx
+++ src/components/SelectModal/index.tsx
@@ -12,22 +12,46 @@ export class SelectModal extends React.Component<{}, SelectModalState>{
     this.state = {
       modalActivated: true,
     }
+
+    this.toggleModal = this.toggleModal.bind(this)
+    this.closeModalSelf = this.closeModalSelf.bind(this)
+  }
+
+  toggleModal () {
+    this.setState({
+      modalActivated: !this.state.modalActivated
+    })
+  }
+
+  closeModalSelf (event: React.MouseEvent<HTMLDivElement>) {
+    if (event.target === event.currentTarget) {
+      this.toggleModal()
+    }
   }
 
   render () {
     const isModalActivated = this.state.modalActivated
     return (
       <div>
-        <div className="modal_button" role="button"></div>
+        <div className="modal_button" role="button" onClick={this.toggleModal}></div>
         { isModalActivated &&
           <ReactCSSTransitionGroup
+            className="modal_window"
             transitionName="modal_window"
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={300}
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={1000}
+            tag="div"
           >
-            <div id="component_modal" is="div">
+            <div
+              id="component_modal"
+              onClick={this.closeModalSelf}
+            >
               <div className="component_modal_inner">
                 <div className="component_modal_content">
+                  <div className="component_modal_close_button"
+                    role="button"
+                    onClick={this.toggleModal}
+                  ></div>
                   <h2 className="component_modal_title">Choose your component</h2>
                 </div>
               </div>
